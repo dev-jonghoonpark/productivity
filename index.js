02245baf7377a908c581bc10b8382271d4a47769
chooco13 <choo_co@naver.com>
2020-07-19 14:08:43 +0900

    add do not disturb

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const { app, BrowserWindow, Tray, Menu, screen, ipcMain } = require('electron')
+const { app, BrowserWindow, Tray, Menu, dialog, screen, ipcMain } = require('electron')
+
+var moment = require('moment');
+
+const Store = require('electron-store');
+const schema = {
+  DO_NOT_DISTURB: {
+    type: 'boolean',
+    default: false
+  }
+}
+const store = new Store({schema});
+
 let ps = require('ps-node');
 
 let win
+let doNotDisturbWin;
 let targetProcess;
+
+let doNotDisturbEnabled;
+let autostartEnabled;
+
 function createWindow () {
   let display = screen.getPrimaryDisplay();
   let width = display.bounds.width;
@@ -22,18 +39,31 @@ function createWindow () {
     frame: false,
     icon: __dirname + '/resources/images/logo.png'
   })
-
   win.setResizable(false)
-
-  // and load the index.html of the app.
   win.loadFile('index.html')
-
   // 개발자 도구를 엽니다.
   // win.webContents.openDevTools()
-
   win.hide();
 
-  //checkProcess();
+  doNotDisturbWin = new BrowserWindow({
+    type:'toolbar',
+    width: 400,
+    height: 220,
+    x: width - 400,
+    y: 24,
+    webPreferences: {
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true
+    },
+    transparent: true, 
+    frame: false,
+    icon: __dirname + '/resources/images/logo.png'
+  })
+  doNotDisturbWin.setResizable(false)
+  doNotDisturbWin.loadFile('do_not_disturb.html')
+  // 개발자 도구를 엽니다.
+  // doNotDisturbWin.webContents.openDevTools()
+  doNotDisturbWin.hide();
 }
 
 // 이 메소드는 Electron의 초기화가 완료되고
@@ -59,29 +89,48 @@ app.on('activate', () => {
 })
 
 let tray = null
+let contextMenu;
 app.on('ready', () => {
   tray = new Tray(__dirname + '/resources/images/tray.png')
-  let checked = app.getLoginItemSettings().openAtLogin;
-  const contextMenu = Menu.buildFromTemplate([
+
+  doNotDisturbEnabled = store.get('DO_NOT_DISTURB');
+  autostartEnabled = app.getLoginItemSettings().openAtLogin;
+
+  contextMenu = Menu.buildFromTemplate([
     {
-      label: '자동실행',
+      label: '방해금지 모드',
       type: 'checkbox',
-      checked: checked,
+      checked: doNotDisturbEnabled,
       click() {
-        if (checked) {
+        doNotDisturbEnabled = store.get('DO_NOT_DISTURB');
+        if (doNotDisturbEnabled) {
+          doNotDisturbEnabled = false;
+          store.set('DO_NOT_DISTURB', doNotDisturbEnabled);
+        } else {
+          contextMenu.items[0].checked = false;
+          doNotDisturbWin.show();
+        }
+      }
+    },
+    {
+      label: '윈도우 시작시 자동실행',
+      type: 'checkbox',
+      checked: autostartEnabled,
+      click() {
+        if (autostartEnabled) {
           app.setLoginItemSettings({
             openAtLogin: false,
             path: app.getPath("exe")
           });
           console.log('disable auto start');
-          checked = false;
+          autostartEnabled = false;
         } else {
           app.setLoginItemSettings({
             openAtLogin: true,
             path: app.getPath("exe")
           });
           console.log('enable auto start');
-          checked = true;
+          autostartEnabled = true;
         }
       }
     },
@@ -99,12 +148,39 @@ app.on('ready', () => {
 // 이 파일에는 나머지 앱의 특정 주요 프로세스 코드를 포함시킬 수 있습니다. 별도의 파일에 추가할 수도 있으며 이 경우 require 구문이 필요합니다.
 
 ipcMain.on('detect', (event, arg) => {
-  win.show();
   targetProcess = arg;
 
-  win.setAlwaysOnTop(true, 'floating')
-  win.setVisibleOnAllWorkspaces(true);
-  win.setFullScreenable(false);
+  if(doNotDisturbEnabled) {
+    let hour = moment().hour();
+    console.log('hour:' + hour);
+    let start = store.get('DO_NOT_DISTURB_START');
+    let end = store.get('DO_NOT_DISTURB_END');
+    console.log({ start: start, end: end });
+    if (hour >= start && hour < end) {
+      if (targetProcess) {
+        const options = {
+          type: 'error',
+          buttons: ['확인'],
+          title: 'FM 억제기',
+          message: '지금은 방해금지 시간입니다.',
+          detail: 'FM을 플레이 하실 수 없습니다.'
+        };
+        dialog.showMessageBox(null, options);
+
+        ps.kill(targetProcess.pid, function (err) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    }
+  } else {
+    win.show();
+    
+    win.setAlwaysOnTop(true, 'floating')
+    win.setVisibleOnAllWorkspaces(true);
+    win.setFullScreenable(false);
+  }
 });
 
 ipcMain.on('cancel', (event, arg) => {
@@ -124,4 +200,23 @@ ipcMain.on('confirm', (event, arg) => {
   }
 
   win.hide();
+})
+
+ipcMain.on('setDoNotDisturb', (event, arg) => {
+  console.log('setDoNotDisturb')
+  console.log(arg);
+
+  doNotDisturbEnabled = true;
+  store.set('DO_NOT_DISTURB', doNotDisturbEnabled);
+  store.set('DO_NOT_DISTURB_START', arg.start);
+  store.set('DO_NOT_DISTURB_END', arg.end);
+  contextMenu.items[0].checked = true;
+
+  doNotDisturbWin.hide();
+})
+
+ipcMain.on('hideDoNotDisturbWin', (event, arg) => {
+  console.log('hideDoNotDisturbWin')
+
+  doNotDisturbWin.hide();
 }) 
